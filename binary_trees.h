d578bdb34c5da76726a64077406ac2a446e31a0c
zed1921 <zewge1921@gmail.com>
2022-02-17 06:25:59 -0800

    heder file

--- binary_trees.h
+++ binary_trees.h
@@ -71,6 +71,7 @@ avl_t *avl_insert(avl_t **tree, int value);
 avl_t *array_to_avl(int *array, size_t size);
 avl_t *avl_remove(avl_t *root, int value);
 avl_t *sorted_array_to_avl(int *array, size_t size);
+avl_t *r_insert_node(avl_t **tree, avl_t *parent, avl_t **new, int nval);
 int binary_tree_is_heap(const binary_tree_t *tree);
 heap_t *heap_insert(heap_t **root, int value);
 heap_t *array_to_heap(int *array, size_t size);
